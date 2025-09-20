bf7e0a97164e06db91d1622cd92694192a060dc3
Ivan Frolov <frolvanya@gmail.com>
2025-09-20 20:02:27 +0900

    refactor: reuse same `amount - withdraw_fee` result

--- bridge-sdk/connectors/omni-connector/src/omni_connector.rs
+++ bridge-sdk/connectors/omni-connector/src/omni_connector.rs
@@ -652,19 +652,17 @@ impl OmniConnector {
 
         let withdraw_fee = near_bridge_client.get_withdraw_fee(chain).await?;
 
+        let net_amount = amount.checked_sub(withdraw_fee).ok_or_else(|| {
+            BridgeSdkError::InvalidArgument("Amount is smaller than `withdraw_fee`".to_string())
+        })?;
+
         let (out_points, utxos_balance, gas_fee) =
-            utxo_utils::choose_utxos(chain, amount - withdraw_fee, utxos, fee_rate)?;
+            utxo_utils::choose_utxos(chain, net_amount, utxos, fee_rate)?;
 
         let change_address = near_bridge_client.get_change_address(chain).await?;
         let tx_outs = utxo_utils::get_tx_outs(
             &target_btc_address,
-            amount
-                .checked_sub(withdraw_fee)
-                .ok_or_else(|| {
-                    BridgeSdkError::InvalidArgument(
-                        "Amount is smaller than `withdraw_fee".to_string(),
-                    )
-                })?
+            net_amount
                 .checked_sub(gas_fee)
                 .ok_or_else(|| {
                     BridgeSdkError::InvalidArgument("Amount is smaller than `gas_fee`".to_string())
