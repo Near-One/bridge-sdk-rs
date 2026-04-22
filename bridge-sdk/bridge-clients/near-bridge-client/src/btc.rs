abcac996a24c203701fd4c0eeae3e593f54fe1a6
kiseln <3428059+kiseln@users.noreply.github.com>
2026-04-23 07:47:21 +0800

    fix

--- bridge-sdk/bridge-clients/near-bridge-client/src/btc.rs
+++ bridge-sdk/bridge-clients/near-bridge-client/src/btc.rs
@@ -32,6 +32,7 @@ const SUBMIT_BTC_TRANSFER_GAS: u64 = 300_000_000_000_000;
 
 const INIT_BTC_TRANSFER_DEPOSIT: u128 = 1;
 const ACTIVE_UTXO_MANAGEMENT_DEPOSIT: u128 = 1;
+const SIGN_BTC_TRANSACTION_DEPOSIT: u128 = 1;
 const BTC_SAFE_VERIFY_DEPOSIT_DEPOSIT: u128 = 1_200_000_000_000_000_000_000;
 const BTC_VERIFY_DEPOSIT_DEPOSIT: u128 = 0;
 const BTC_VERIFY_WITHDRAW_DEPOSIT: u128 = 0;
@@ -259,7 +260,7 @@ impl NearBridgeClient {
                 .to_string()
                 .into_bytes(),
                 gas: SIGN_BTC_TRANSACTION_GAS,
-                deposit: 1,
+                deposit: SIGN_BTC_TRANSACTION_DEPOSIT,
             },
             transaction_options.wait_until,
             transaction_options.wait_final_outcome_timeout_sec,
