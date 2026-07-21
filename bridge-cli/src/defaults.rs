1a73b50c8389a32592076955bf3793cb2261d71a
Ivan Frolov <frolvanya@gmail.com>
2026-07-21 16:39:18 +0200

    chore: populated aptos bridge addresses

--- bridge-cli/src/defaults.rs
+++ bridge-cli/src/defaults.rs
@@ -73,9 +73,8 @@ pub const STARKNET_BRIDGE_TOKEN_FACTORY_ADDRESS_MAINNET: &str =
 pub const STARKNET_CHAIN_ID_MAINNET: &str = "SN_MAIN";
 
 pub const APTOS_RPC_MAINNET: &str = "https://fullnode.mainnet.aptoslabs.com/v1";
-// TODO: set once the omni_bridge Move package is deployed to Aptos mainnet.
 pub const APTOS_BRIDGE_TOKEN_FACTORY_ADDRESS_MAINNET: &str =
-    "0x0000000000000000000000000000000000000000000000000000000000000000";
+    "0xe4ec15f237e5a8c7daa7a34ece28e2bd2c079360763f181bf65862ec148b914a";
 
 pub const FOGO_RPC_MAINNET: &str = "https://mainnet.fogo.io";
 pub const FOGO_BRIDGE_ADDRESS_MAINNET: &str = "dahPEoZGXfyV58JqqH85okdHmpN8U2q8owgPUXSCPxe";
@@ -155,7 +154,6 @@ pub const STARKNET_BRIDGE_TOKEN_FACTORY_ADDRESS_TESTNET: &str =
 pub const STARKNET_CHAIN_ID_TESTNET: &str = "SN_SEPOLIA";
 
 pub const APTOS_RPC_TESTNET: &str = "https://fullnode.testnet.aptoslabs.com/v1";
-// TODO: set once the omni_bridge Move package is deployed to Aptos testnet.
 pub const APTOS_BRIDGE_TOKEN_FACTORY_ADDRESS_TESTNET: &str =
     "0x904a7d620944eec42d5d46cf4fe12463f713c8a705d581c10a010672228f967c";
 
@@ -237,7 +235,6 @@ pub const STARKNET_BRIDGE_TOKEN_FACTORY_ADDRESS_DEVNET: &str =
 pub const STARKNET_CHAIN_ID_DEVNET: &str = "SN_SEPOLIA";
 
 pub const APTOS_RPC_DEVNET: &str = "https://fullnode.testnet.aptoslabs.com/v1";
-// TODO: set once the omni_bridge Move package is deployed.
 pub const APTOS_BRIDGE_TOKEN_FACTORY_ADDRESS_DEVNET: &str =
     "0x0000000000000000000000000000000000000000000000000000000000000000";
 
